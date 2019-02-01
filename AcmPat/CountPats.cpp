7b2c083ab992994340342a2a9e1ffc116f973ae0
HongwuZ <957130821@qq.com>
2019-02-01 10:56:54 +0800

    1093 Count PAT's 参考柳神  :thumbsup:

---
+++ AcmPat/CountPats.cpp
@@ -0,0 +1,27 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#define LOCAL
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+#endif
+	int pCnt = 0, tCnt = 0, res = 0;
+	string str1;
+	cin >> str1;
+	for (int i = 0; i < str1.size(); i++) 
+		if (str1[i] == 'T')
+			tCnt++;
+	for (int i = 0; i < str1.size(); i++) {
+		if (str1[i] == 'P')
+			pCnt++;
+		else if (str1[i] == 'T')
+			tCnt--;
+		else
+			res = (res + pCnt * tCnt) % 1000000007;
+	}
+	cout << res;
+	return 0;
+}
